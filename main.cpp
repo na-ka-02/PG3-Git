98de28b7e05cd928ae48cd9e8b0f392f355e7f53
na <k023g1066@g.neec.ac.jp>
2024-05-17 16:49:37 +0900

    確認課題の03-01をやった

--- main.cpp
+++ main.cpp
@@ -1,36 +1,46 @@
 #include <iostream>
 #include <list>
+#include<stdio.h>
 
-// 山手線の駅名をchar*型の配列で定義
-const char* station_names[] = {
-	"Tokyo", "Kanda", "Akihabara", "Okachimachi", "Ueno", "Uguisudani",
-	"Nippori", "Nishi-Nippori", "Tabata", "Komagome", "Sugama", "Otsuka",
-	"Mejiro", "Takadanobaba", "Shin-Okubo", "Shinjuku","Yoyogi","Harajuku",
-	"Shibuya", "Ebisu", "Meguro", "Gotanda", "Osaki", "Shinagawa",
-	"Takanawa Gateway","Tamati", "Hamamatsucho", "Shimbashi", "Yurakucho",
-};
+int main()
+{
+	std::list<const char*>stationList
+	{
+		//山手線の駅名をまとめた双方向リスト
+		"Tokyo", "Kanda", "Akihabara", "Okachimachi", "Ueno", "Uguisudani",
+		"Nippori", "Tabata", "Komagome", "Sugama", "Otsuka",
+		"Mejiro", "Takadanobaba", "Shin-Okubo", "Shinjuku","Yoyogi","Harajuku",
+		"Shibuya", "Ebisu", "Meguro", "Gotanda", "Osaki", "Shinagawa",
+		"Tamati", "Hamamatsucho", "Shimbashi", "Yurakucho"
+	};
 
-// 双方向リストの型エイリアス
-using StationList = std::list<const char*>;
+	printf("\n1970年の山手線の駅名\n");
+	for (std::list<const char*>::iterator itr = stationList.begin(); itr != stationList.end(); ++itr)
+	{
+		if (*itr == "Tabata")
+		{
+			itr = stationList.insert(itr, "Nishi-Nippori");
+			++itr;
+		}
+		std::cout << *itr << std::endl;
+	}
 
-// 指定した年の山手線の駅リストを表示する関数
-void displayStationList(const StationList& stations, int year) {
-	std::cout << "=== " << year << "年の山手線の駅リスト ===" << std::endl;
-	for(std::list<char>::iterator)
-	std::cout << std::endl;
-}
+	printf("\n2019年の山手線の駅名\n");
+	for (std::list<const char*>::iterator itr = stationList.begin(); itr != stationList.end(); ++itr)
+	{
+		if(*itr=="Shinagawa")
+		{
+			itr = stationList.insert(itr, "Takanawa Gateway");
+			++itr;
+		}
+		std::cout << *itr << std::endl;
+	}
 
-int main() {
-	// 1970年、2019年、2022年の山手線の駅リストを作成
-	StationList stations_1970(station_names, station_names + sizeof(station_names) / sizeof(station_names[0]));
-	StationList stations_2019(station_names, station_names + sizeof(station_names) / sizeof(station_names[0]));
-	StationList stations_2022(station_names, station_names + sizeof(station_names) / sizeof(station_names[0]));
-
-	// それぞれの時点での駅リストを表示
-	displayStationList(stations_1970, 1970);
-	displayStationList(stations_2019, 2019);
-	displayStationList(stations_2022, 2022);
-
+	printf("\n2022年の山手線の駅名\n");
+	for (std::list<const char*>::iterator itr = stationList.begin(); itr != stationList.end(); ++itr)
+	{
+		std::cout << *itr << std::endl;
+	}
 	return 0;
 }
 
