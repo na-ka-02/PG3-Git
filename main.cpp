b2e60f97adf2745ee61a5abcc9385304b1f6836e
na <k023g1066@g.neec.ac.jp>
2024-06-21 17:12:17 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -1,34 +1,5 @@
 #include <stdio.h>
-#include"DeathEater.h"
-#include"Load.h"
-#include"Dementor.h"
 
 int main()
 {
-	DeathEater* death_eaters[4];
-
-	//生成フェーズ
-	for (int i = 0; i < 2; i++)
-	{
-		if (i < 1)
-
-			death_eaters[i] = new Dementor;
-
-		else
-
-			death_eaters[i] = new Load;
-
-	}
-
-	//攻撃フェーズ
-	for (int i = 0; i < 2; i++)
-	{
-		death_eaters[i]->Attack();
-	}
-	//破棄フェーズ
-	for (int i = 0; i < 2; i++)
-	{
-		delete death_eaters[i];
-	}
-	return 0;
 }
