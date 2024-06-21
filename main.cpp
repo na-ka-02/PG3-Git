be32781cfcff47a3160c0069b3e936849b389daf
na <k023g1066@g.neec.ac.jp>
2024-06-21 16:52:17 +0900

    直した

--- main.cpp
+++ main.cpp
@@ -5,43 +5,27 @@
 
 int main()
 {
-<<<<<<< Updated upstream
-	FriendEnjoy* death_eaters[4];
-=======
 	MeetFriend* meet_friend[4];
->>>>>>> Stashed changes
 
 	//生成フェーズ
 	for (int i = 0; i < 2; i++)
 	{
 		if (i < 1)
-<<<<<<< Updated upstream
 		{
-			death_eaters[i] = new NoFriend;
-		}
-		else
-		{
-			death_eaters[i] = new FriendAngrey;
-		}
-=======
-
 			meet_friend[i] = new MeetFriend;
-
+		}
 		else
-
+		{
 			meet_friend[i] = new DontMeetFriend;
 
->>>>>>> Stashed changes
+		}
+
 	}
 
 	//攻撃フェーズ
 	for (int i = 0; i < 2; i++)
 	{
-<<<<<<< Updated upstream
-		death_eaters[i]->FriendOver();
-=======
 		meet_friend[i]->Attack();
->>>>>>> Stashed changes
 	}
 	//破棄フェーズ
 	for (int i = 0; i < 2; i++)
